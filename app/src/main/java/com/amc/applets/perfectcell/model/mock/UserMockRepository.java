bacb97b35062048577039cbcc14f5230f621dbf4
Joshua Ghali <jean-joshua.ghali.1@etsmtl.net>
2016-02-13 02:09:54 -0500

    modification de gradle pour heroku

---
+++ app/src/main/java/com/amc/applets/perfectcell/model/mock/UserMockRepository.java
@@ -0,0 +1,7 @@
+package com.amc.applets.perfectcell.model.mock;
+
+/**
+ * Created by jjgha on 2016-02-12.
+ */
+public class UserMockRepository {
+}
---
+++ app/src/main/java/com/amc/applets/perfectcell/model/repositories/ICrudRepository.java
@@ -0,0 +1,11 @@
+package com.amc.applets.perfectcell.model.repositories;
+
+/**
+ * Created by jjgha on 2016-02-12.
+ */
+public interface ICrudRepository {
+    T Add(T item);
+    T Edit (T item);
+    T List();
+    T Get(int id);
+}
