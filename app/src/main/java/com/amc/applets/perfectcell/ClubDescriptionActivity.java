4396e71aca6f31423a4d88cc7f3a0c29dacb8054
sdetilly <steven.detilly@gmail.com>
2016-02-13 05:49:36 -0500

    custom SnackBar works

--- app/src/main/java/com/amc/applets/perfectcell/ClubDescriptionActivity.java
+++ app/src/main/java/com/amc/applets/perfectcell/ClubDescriptionActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -29,20 +32,32 @@ public class ClubDescriptionActivity extends AppCompatActivity {
         clubName = extras.getString("clubName");
         setTitle(clubName);
 
-        Snackbar.make(coordinatorLayout, getResources().getString(R.string.club_snackbar) + " " + clubName, Snackbar.LENGTH_INDEFINITE)
-                .setAction("Envoyer courriel", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(Intent.ACTION_SENDTO);
-                        intent.setData(Uri.parse("mailto:" + clubName + "@ens.etsmtl.ca"));
-                        intent.putExtra(Intent.EXTRA_SUBJECT, "Aide pour le club");
-                        intent.putExtra(Intent.EXTRA_TEXT, "Bonjour, nous aimerions soutenir votre demarche. " +
-                                "SVP Comuniquer avec nous pour plus d'information.");
+        Snackbar snackbar = Snackbar.make(coordinatorLayout, "", Snackbar.LENGTH_LONG);
+        // Get the Snackbar's layout view
+        Snackbar.SnackbarLayout layout = (Snackbar.SnackbarLayout) snackbar.getView();
+        // Hide the text
+        TextView textView = (TextView) layout.findViewById(android.support.design.R.id.snackbar_text);
+        //textView.setVisibility(View.INVISIBLE);
+        textView.setText(getString(R.string.club_snackbar) + " " + clubName);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        layout.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        layout.setClickable(true);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:" + clubName + "@ens.etsmtl.ca"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Aide pour le club");
+                intent.putExtra(Intent.EXTRA_TEXT, "Bonjour, nous aimerions soutenir votre demarche. " +
+                        "SVP Communiquer avec nous pour plus d'information.");
 
-                        startActivityForResult(Intent.createChooser(intent, "Choose an Email client:"), 1);
-                    }
-                })
-                .show();
+                startActivityForResult(Intent.createChooser(intent, "Choose an Email client:"), 1);
+            }
+        });
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(24);
+        snackbar.setDuration(Snackbar.LENGTH_INDEFINITE).show();
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
