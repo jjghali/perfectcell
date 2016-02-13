345b36bef8add863e26011d95b16e26ccd6b20e0
yvesmillette <yves.millette.2@etsmtl.net>
2016-02-13 02:37:15 -0500

    added base code for retrofit + http client to retrieve json

---
+++ app/src/main/java/com/amc/applets/perfectcell/ApiClient/ApiInterface.java
@@ -0,0 +1,33 @@
+package com.amc.applets.perfectcell.ApiClient;
+
+import com.amc.applets.perfectcell.model.Giver;
+import com.amc.applets.perfectcell.model.Receiver;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
+/**
+ * Created by Yves on 2/13/2016.
+ * Interface class defining endpoints and methods for data access.
+ */
+public interface ApiInterface {
+
+    @Headers("User-Agent: PerfectCell-AMC2016")
+    @GET("/receivers/")
+    Call<List<Receiver>> getReceivers();
+
+    @GET("/receivers/{id}")
+    Call<Receiver> getReceiversById(@Query("q") String id);
+
+    @GET("/givers/")
+    Call<List<Giver>> getGivers();
+
+    @GET("/givers/(id}")
+    Call<Giver> getGiversById(@Query("q") String id);
+
+
+}
